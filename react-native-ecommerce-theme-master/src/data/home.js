7ec09e45fd7fb062b09830ea9bf92818f08de521
Katy\Katy Tong <katytong0420@gmail.com>
2019-02-24 01:59:34 +0800

    edit dummy data with new logo

--- react-native-ecommerce-theme-master/src/data/home.js
+++ react-native-ecommerce-theme-master/src/data/home.js
@@ -7,25 +7,25 @@ const homeData = {
             {
                 id: 1,
                 name: 'Hummingbird Watch',
-                imageUri: 'https://s-media-cache-ak0.pinimg.com/564x/75/4e/47/754e472b37cc1d8864ecd5ed67e1aa92.jpg',
+                imageUri: require('.././assets/dummy_icon/1.png'),
                 prize: 128
             },
             {
                 id: 2,
                 name: 'metro - chalkboard leather strap watch, 34mm',
-                imageUri: 'https://s-media-cache-ak0.pinimg.com/564x/9a/6c/59/9a6c59b0fd1a81edc282e8848acee34d.jpg',
+                imageUri: require('.././assets/dummy_icon/2.png'),
                 prize: 195
             },
             {
                 id: 3,
                 name: 'Olivia Burton Exclusive Floral Big Dial Watch',
-                imageUri: 'https://s-media-cache-ak0.pinimg.com/564x/75/4e/47/754e472b37cc1d8864ecd5ed67e1aa92.jpg',
+                imageUri: require('.././assets/dummy_icon/3.png'),
                 prize: 96
             },
             {
                 id: 4,
                 name: 'Olivia Burton Enchanted Garden Gray Patent Big Dial Watch',
-                imageUri: 'https://s-media-cache-ak0.pinimg.com/564x/78/f7/17/78f71796c66a31fbd136efb784a9632d.jpg',
+                imageUri: require('.././assets/dummy_icon/4.png'),
                 prize: 96
             },
         ]
@@ -38,25 +38,25 @@ const homeData = {
             {
                 id: 1,
                 name: 'Hummingbird Watch',
-                imageUri: 'https://c1.staticflickr.com/6/5062/5649039693_4ae12562b5_b.jpg',
+                imageUri: require('.././assets/dummy_icon/5.png'),
                 prize: 128
             },
             {
                 id: 2,
                 name: 'metro - chalkboard leather strap watch, 34mm',
-                imageUri: 'https://c1.staticflickr.com/8/7014/6712827125_4e1e474737_b.jpg',
+                imageUri: require('.././assets/dummy_icon/6.png'),
                 prize: 195
             },
             {
                 id: 3,
                 name: 'Olivia Burton Exclusive Floral Big Dial Watch',
-                imageUri: 'https://c1.staticflickr.com/6/5545/10770069154_b626552554_b.jpg',
+                imageUri: require('.././assets/dummy_icon/7.png'),
                 prize: 96
             },
             {
                 id: 4,
                 name: 'Olivia Burton Enchanted Garden Gray Patent Big Dial Watch',
-                imageUri: 'https://c1.staticflickr.com/9/8142/6974554082_5b5a7ef762_z.jpg',
+                imageUri: require('.././assets/dummy_icon/8.png'),
                 prize: 96
             },
         ]
@@ -69,27 +69,15 @@ const homeData = {
             {
                 id: 1,
                 name: 'Hummingbird Watch',
-                imageUri: 'https://c1.staticflickr.com/3/2337/1637193093_f5661532b0_z.jpg?zz=1',
+                imageUri: require('.././assets/dummy_icon/9.png'),
                 prize: 12800
             },
             {
                 id: 2,
                 name: 'metro - chalkboard leather strap watch, 34mm',
-                imageUri: 'https://c1.staticflickr.com/3/2272/2102183091_fb38bb3822.jpg',
+                imageUri: require('.././assets/dummy_icon/10.png'),
                 prize: 195
-            },
-            {
-                id: 3,
-                name: 'Olivia Burton Exclusive Floral Big Dial Watch',
-                imageUri: 'https://c1.staticflickr.com/7/6109/6311668225_673b9e1e3c_z.jpg',
-                prize: 96
-            },
-            {
-                id: 4,
-                name: 'Olivia Burton Enchanted Garden Gray Patent Big Dial Watch',
-                imageUri: 'https://c2.staticflickr.com/4/3311/3670965640_da4a67be4f_z.jpg?zz=1',
-                prize: 96
-            },
+            }
         ]
     },
 
@@ -100,7 +88,7 @@ const homeData = {
             {
                 id: 1,
                 name: 'Earing',
-                imageUri: 'https://images-na.ssl-images-amazon.com/images/I/91pV8dVwtFL._SL420_SX500_CR0,0,520,500.jpg',
+                imageUri: require('.././assets/dummy_icon/1.png'),
                 prize: 80,
                 regularPrize: 100.00,
                 discountPercent: 20
@@ -108,7 +96,7 @@ const homeData = {
             {
                 id: 2,
                 name: 'iPhone 7 Plus 6S Plus 6 Plus Screen Protector',
-                imageUri: 'https://images-na.ssl-images-amazon.com/images/I/71RHBxJlqUL._SX522_.jpg',
+                imageUri: require('.././assets/dummy_icon/2.png'),
                 prize: 7.99,
                 regularPrize: 29.99,
                 discountPercent: 73
@@ -116,7 +104,7 @@ const homeData = {
             {
                 id: 3,
                 name: 'iPhone 7 Plus 6S Plus 6 Plus Screen Protector,',
-                imageUri: 'https://images-na.ssl-images-amazon.com/images/I/71F29HsHp2L._SX466_.jpg',
+                imageUri: require('.././assets/dummy_icon/3.png'),
                 prize: 30.10,
                 regularPrize: 39.99,
                 discountPercent: 25
@@ -124,7 +112,7 @@ const homeData = {
             {
                 id: 4,
                 name: 'Skip Hop Moby Bath Tear-Free Waterfall Rinser, Blue',
-                imageUri: 'https://images-na.ssl-images-amazon.com/images/I/81q648i%2BXgL._SX522_.jpg',
+                imageUri: require('.././assets/dummy_icon/4.png'),
                 prize: 8.00,
                 regularPrize: 10.00,
                 discountPercent: 20
@@ -139,7 +127,7 @@ const homeData = {
             {
                 id: 1,
                 name: 'Earing',
-                imageUri: 'https://c1.staticflickr.com/6/5062/5649039693_4ae12562b5_b.jpg',
+                imageUri: require('.././assets/dummy_icon/5.png'),
                 prize: 80,
                 regularPrize: 100.00,
                 discountPercent: 20
@@ -147,7 +135,7 @@ const homeData = {
             {
                 id: 2,
                 name: 'iPhone 7 Plus 6S Plus 6 Plus Screen Protector',
-                imageUri: 'https://c1.staticflickr.com/8/7014/6712827125_4e1e474737_b.jpg',
+                imageUri: require('.././assets/dummy_icon/6.png'),
                 prize: 7.99,
                 regularPrize: 29.99,
                 discountPercent: 73
@@ -155,15 +143,14 @@ const homeData = {
             {
                 id: 3,
                 name: 'iPhone 7 Plus 6S Plus 6 Plus Screen Protector,',
-                imageUri: 'https://c1.staticflickr.com/6/5545/10770069154_b626552554_b.jpg',
-                prize: 30.10,
+                imageUri: require('.././assets/dummy_icon/7.png'),
                 regularPrize: 39.99,
                 discountPercent: 25
             },
             {
                 id: 4,
                 name: 'Skip Hop Moby Bath Tear-Free Waterfall Rinser, Blue',
-                imageUri: 'https://c1.staticflickr.com/9/8142/6974554082_5b5a7ef762_z.jpg',
+                imageUri: require('.././assets/dummy_icon/8.png'),
                 prize: 8.00,
                 regularPrize: 10.00,
                 discountPercent: 20
